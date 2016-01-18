16bc86140b7aef0fa32f706ef0d6d40fa3a012f8
Stefan Majoor <stefan@codeyellow.nl>
2016-01-18 18:51:05 +0100

    commented tests

--- test/algorithms/Sorting/SortableNGTest.java
+++ test/algorithms/Sorting/SortableNGTest.java
@@ -29,6 +29,10 @@ abstract public class SortableNGTest {
     
 
     // UNIT TESTS
+    
+    /**
+     * A first simple test that test if a set is sorted
+     */
     @Test
     public void testSimpleSort() {
         Integer[] set = {5, 3, 1, 2, 4};
@@ -48,7 +52,10 @@ abstract public class SortableNGTest {
         assertArrayEquals(expected, sorted);
     }
     
-   @Test
+    /**
+     * Test that the set is correctly sorted if the array needs to be sorted in reverse
+     */
+    @Test
     public void testSimpleSortReverse() {
         Integer[] set = {5, 3, 1, 2, 4};
         Integer[] expected = {5, 4, 3, 2, 1};
@@ -67,6 +74,9 @@ abstract public class SortableNGTest {
         assertArrayEquals(expected, sorted);
     }
     
+    /**
+     * Test if a set is sorted correctly if it was already sorted
+     */
     @Test
     public void testSortedSet() {
         Integer[] set = {1, 2, 3, 4, 5};
@@ -86,6 +96,9 @@ abstract public class SortableNGTest {
         assertArrayEquals(expected, sorted);
     }
     
+    /**
+     * Test if a set is sorted if the values are given in totally the reverse order
+     */
     @Test
     public void testTotallyUnSortedSet() {
         Integer[] set = {5, 4, 3, 2, 1};
@@ -105,6 +118,9 @@ abstract public class SortableNGTest {
         assertArrayEquals(expected, sorted);
     }
     
+    /**
+     * Test sorting the empty set
+     */
     @Test
     public void testEmptySet() {
         Integer[] set = {};
@@ -125,6 +141,9 @@ abstract public class SortableNGTest {
         assertArrayEquals(expected, sorted);
     }
     
+    /**
+     * Test that sorting is correct if we sort on something else than numbers
+     */
     @Test
     public void testNonInteger() {
         Foo one = new Foo(1);
@@ -139,6 +158,37 @@ abstract public class SortableNGTest {
                 return -1;
             }
             if (o1.getValue() < o2.getValue()) {
+                return 1;
+            }
+            return 0;
+        };
+        
+        Foo[] sorted = sorter.sort(set, comp);
+        
+        assertArrayEquals(expected, sorted);
+    }
+    
+    /**
+     * Test that the sorting algorithm sorts correctly if null values are present
+     */
+    @Test
+    public void testNull() {
+        Foo one = null;
+        Foo two = new Foo(2);
+        Foo three = new Foo(3);
+        
+        Foo[] set = {three, one, two};
+        Foo[] expected = {one, two, three};
+        
+        Comparator<Foo> comp = (Foo o1, Foo o2) -> {
+            if (o1 == null && o2 == null) {
+                return 0;
+            }
+            
+            if (o1 == null ||  o1.getValue() < o2.getValue()) {
+                return -1;
+            }
+            if (o2 == null || o1.getValue() < o2.getValue()) {
                 return 1;
             }
             return 0;
